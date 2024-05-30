f56860940c48abcc5e5d49dc8f4f1e16c504a126
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.gitlab.com>
2024-05-30 20:02:38 +0300

    Change target framework.

--- FibonacciIterator/FibonacciEnumerable.cs
+++ FibonacciIterator/FibonacciEnumerable.cs
@@ -1,14 +1,15 @@
-﻿namespace FibonacciIterator
-{
-    /// <summary>
-    /// Represents an enumerable object to iterate over the Fibonacci sequence numbers.
-    /// </summary>
-    public sealed class FibonacciEnumerable
-    {
-        // TODO Implement an enumerable to iterate over the Fibonacci sequence numbers.
+﻿using System.Collections;
 
-        public FibonacciEnumerable(int count = int.MaxValue, int skipCount = 0)
-        {
-        }
+namespace FibonacciIterator;
+
+/// <summary>
+/// Represents an enumerable object to iterate over the Fibonacci sequence numbers.
+/// </summary>
+public sealed class FibonacciEnumerable
+{
+    // TODO Implement an enumerable to iterate over the Fibonacci sequence numbers.
+
+    public FibonacciEnumerable(int count = int.MaxValue, int skipCount = 0)
+    {
     }
 }
