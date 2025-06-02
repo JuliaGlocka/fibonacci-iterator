cf6ecb7e0c02a70acdbd8f9a4ada2c3a0a477e2d
glock <glockajulia@gmail.com>
2025-06-02 22:03:24 +0200

    solution v5

--- FibonacciIterator/FibonacciEnumerator.cs
+++ FibonacciIterator/FibonacciEnumerator.cs
@@ -6,18 +6,20 @@ namespace FibonacciIterator;
 
 public sealed class FibonacciEnumerator : IEnumerator<int>
 {
-    // Only int fields!
-    private int _a, _b;         // Fibonacci state
-    private int _index;         // How many in sequence have we advanced (including skips)
-    private int _yielded;       // How many have we yielded to the user
-    private int _count;         // How many to yield to the user
-    private int _skipCount;     // How many to skip at the start
-    private int _started;       // 0 = not started, 1 = started, -1 = finished
+    // Fields
+    private int _a, _b;
+    private int _index;
+    private int _yielded;
+    private int _count;
+    private int _skipCount;
+    private int _maxToYield;
+    private int _started; // 0 = not started, 1 = started, -1 = finished
 
     public FibonacciEnumerator(int count, int skipCount)
     {
         _count = count;
         _skipCount = skipCount;
+        _maxToYield = Math.Max(0, count - skipCount);
         Reset();
     }
 
@@ -35,47 +37,30 @@ public sealed class FibonacciEnumerator : IEnumerator<int>
 
     public bool MoveNext()
     {
-        if (_started == -1)
-            return false;
-
-        if (_started == 0)
-        {
-            // Start and skip skipCount items
-            _a = 0;
-            _b = 1;
-            _index = 0;
-            _yielded = 0;
-
-            while (_index < _skipCount)
-            {
-                int temp = _a + _b;
-                _a = _b;
-                _b = temp;
-                _index++;
-            }
-
-            if (_count == 0)
-            {
-                _started = -1;
-                return false;
-            }
-
-            _started = 1;
-        }
-        else
-        {
-            // Advance to next
-            int temp = _a + _b;
-            _a = _b;
-            _b = temp;
-        }
-
-        if (_yielded >= _count)
+        if (_yielded >= _maxToYield)
         {
             _started = -1;
             return false;
         }
-
+        if (_started == 0)
+        {
+            _a = 0;
+            _b = 1;
+            _index = 0;
+            for (int i = 0; i < _skipCount; i++)
+            {
+                int temp = _a + _b;
+                _a = _b;
+                _b = temp;
+            }
+            _started = 1;
+        }
+        else
+        {
+            int temp = _a + _b;
+            _a = _b;
+            _b = temp;
+        }
         _yielded++;
         return true;
     }
